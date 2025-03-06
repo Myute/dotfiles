0e3e882b08a521bbc618ab585887f8b9654baad2
Kevyn Higbee <kevynhigbee@gmail.com>
2025-03-06 06:38:21 -0800

    changed crt shader to one without virtual screen warping

--- ghostty/shaders/crt.glsl
+++ ghostty/shaders/crt.glsl
@@ -1,310 +1,189 @@
-// source: https://gist.github.com/qwerasd205/c3da6c610c8ffe17d6d2d3cc7068f17f
-// credits: https://github.com/qwerasd205
-//==============================================================
-//
-//    [CRTS] PUBLIC DOMAIN CRT-STYLED SCALAR by Timothy Lottes
-//
-//    [+] Adapted with alterations for use in Ghostty by Qwerasd.
-//    For more information on changes, see comment below license.
-//
-//==============================================================
-//
-//      LICENSE = UNLICENSE (aka PUBLIC DOMAIN)
-//
-//--------------------------------------------------------------
-// This is free and unencumbered software released into the
-// public domain.
-//--------------------------------------------------------------
-// Anyone is free to copy, modify, publish, use, compile, sell,
-// or distribute this software, either in source code form or as
-// a compiled binary, for any purpose, commercial or
-// non-commercial, and by any means.
-//--------------------------------------------------------------
-// In jurisdictions that recognize copyright laws, the author or
-// authors of this software dedicate any and all copyright
-// interest in the software to the public domain. We make this
-// dedication for the benefit of the public at large and to the
-// detriment of our heirs and successors. We intend this
-// dedication to be an overt act of relinquishment in perpetuity
-// of all present and future rights to this software under
-// copyright law.
-//--------------------------------------------------------------
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-// AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
-// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-//--------------------------------------------------------------
-// For more information, please refer to
-// <http://unlicense.org/>
-//==============================================================
+//-----------------------------------------------------------------------------
+// CRT Shader with Chromatic Aberration, Glow, Scanlines, Dot Matrix and Transparency
+//-----------------------------------------------------------------------------
 
-// This shader is a modified version of the excellent
-// FixingPixelArtFast by Timothy Lottes on Shadertoy.
-//
-// The original shader can be found at:
-// https://www.shadertoy.com/view/MtSfRK
-//
-// Modifications have been made to reduce the verbosity,
-// and many of the comments have been removed / reworded.
-// Additionally, the license has been moved to the top of
-// the file, and can be read above. I (Qwerasd) choose to
-// release the modified version under the same license.
+//-----------------------------------------------------------------------------
+// Customizable Parameters
+//-----------------------------------------------------------------------------
 
-// The appearance of this shader can be altered
-// by adjusting the parameters defined below.
+// Transparency Settings
+const float GLOBAL_OPACITY = 1;        // Overall transparency (1.0 = fully opaque, 0.0 = fully transparent)
 
-// "Scanlines" per real screen pixel.
-// e.g. SCALE 0.5 means each scanline is 2 pixels.
-// Recommended values:
-//  o High DPI displays: 0.33333333
-//  - Low DPI displays:  0.66666666
-#define SCALE 0.33333333
+// Chromatic Aberration Settings
+const float ABBERATION_FACTOR = 0.025;    // Strength of color splitting effect (default: 0.05)
 
-// "Tube" warp
-#define CRTS_WARP 1
+// Glow/Bloom Settings
+const float DIM_CUTOFF = 0.28;           // Threshold for what's considered a dim pixel (default: 0.35)
+const float BRIGHT_CUTOFF = 0.65;        // Threshold for what's considered a bright pixel
+const float BRIGHT_BOOST = 1.0;          // Brightness multiplier for bright pixels (default: 1.2)
+const float DIM_GLOW = 0.05;             // Glow intensity for dim pixels (default: 0.05)
+const float BRIGHT_GLOW = 0.10;          // Glow intensity for bright pixels (default: 0.10)
+const float COLOR_GLOW = 0.3;            // Color bleeding intensity (default: 0.3)
 
-// Darkness of vignette in corners after warping
-//  0.0 = completely black
-//  1.0 = no vignetting
-#define MIN_VIN 0.5
+// Scanline Settings
+const float SCANLINE_INTENSITY = 1.0;    // Overall intensity of scanlines (default: 1.0)
+const float SCANLINE_DENSITY = 0.25;     // Density/thickness of scanlines (default: 0.25)
 
-// Try different masks
-// #define CRTS_MASK_GRILLE 1
-// #define CRTS_MASK_GRILLE_LITE 1
-// #define CRTS_MASK_NONE 1
-#define CRTS_MASK_SHADOW 1
+// Dot Matrix Settings
+const float MASK_INTENSITY = 0;          // Strength of the dot pattern (default: 0.3)
+const float MASK_SIZE = 1.0;             // Size of the dots (default: 1.0)
 
-// Scanline thinness
-//  0.50 = fused scanlines
-//  0.70 = recommended default
-//  1.00 = thinner scanlines (too thin)
-#define INPUT_THIN 0.75
+//-----------------------------------------------------------------------------
+// Color Space Conversion Functions
+//-----------------------------------------------------------------------------
 
-// Horizonal scan blur
-//  -3.0 = pixely
-//  -2.5 = default
-//  -2.0 = smooth
-//  -1.0 = too blurry
-#define INPUT_BLUR -2.75
-
-// Shadow mask effect, ranges from,
-//  0.25 = large amount of mask (not recommended, too dark)
-//  0.50 = recommended default
-//  1.00 = no shadow mask
-#define INPUT_MASK 0.65
-
-float FromSrgb1(float c) {
-  return (c <= 0.04045) ? c * (1.0 / 12.92) :
-  pow(c * (1.0 / 1.055) + (0.055 / 1.055), 2.4);
-}
-vec3 FromSrgb(vec3 c) {
-  return vec3(
-    FromSrgb1(c.r), FromSrgb1(c.g), FromSrgb1(c.b));
+// sRGB linear -> nonlinear transform
+float f(float x) {
+    if (x >= 0.0031308) return 1.055 * pow(x, 1.0 / 2.4) - 0.055;
+    return 12.92 * x;
 }
 
-vec3 CrtsFetch(vec2 uv) {
-  return FromSrgb(texture(iChannel0, uv.xy).rgb);
+float f_inv(float x) {
+    if (x >= 0.04045) return pow((x + 0.055) / 1.055, 2.4);
+    return x / 12.92;
 }
 
-#define CrtsRcpF1(x) (1.0/(x))
-#define CrtsSatF1(x) clamp((x),0.0,1.0)
-
-float CrtsMax3F1(float a, float b, float c) {
-  return max(a, max(b, c));
+// Oklab color space conversions
+vec4 toOklab(vec4 rgb) {
+    vec3 c = vec3(f_inv(rgb.r), f_inv(rgb.g), f_inv(rgb.b));
+    float l = 0.4122214708 * c.r + 0.5363325363 * c.g + 0.0514459929 * c.b;
+    float m = 0.2119034982 * c.r + 0.6806995451 * c.g + 0.1073969566 * c.b;
+    float s = 0.0883024619 * c.r + 0.2817188376 * c.g + 0.6299787005 * c.b;
+    float l_ = pow(l, 1.0 / 3.0);
+    float m_ = pow(m, 1.0 / 3.0);
+    float s_ = pow(s, 1.0 / 3.0);
+    return vec4(
+        0.2104542553 * l_ + 0.7936177850 * m_ - 0.0040720468 * s_,
+        1.9779984951 * l_ - 2.4285922050 * m_ + 0.4505937099 * s_,
+        0.0259040371 * l_ + 0.7827717662 * m_ - 0.8086757660 * s_,
+        rgb.a
+    );
 }
 
-vec2 CrtsTone(
-  float thin,
-  float mask) {
-  #ifdef CRTS_MASK_NONE
-  mask = 1.0;
-  #endif
-
-  #ifdef CRTS_MASK_GRILLE_LITE
-  // Normal R mask is {1.0,mask,mask}
-  // LITE   R mask is {mask,1.0,1.0}
-  mask = 0.5 + mask * 0.5;
-  #endif
-
-  vec2 ret;
-  float midOut = 0.18 / ((1.5 - thin) * (0.5 * mask + 0.5));
-  float pMidIn = 0.18;
-  ret.x = ((-pMidIn) + midOut) / ((1.0 - pMidIn) * midOut);
-  ret.y = ((-pMidIn) * midOut + pMidIn) / (midOut * (-pMidIn) + midOut);
-
-  return ret;
+vec4 toRgb(vec4 oklab) {
+    vec3 c = oklab.rgb;
+    float l_ = c.r + 0.3963377774 * c.g + 0.2158037573 * c.b;
+    float m_ = c.r - 0.1055613458 * c.g - 0.0638541728 * c.b;
+    float s_ = c.r - 0.0894841775 * c.g - 1.2914855480 * c.b;
+    float l = l_ * l_ * l_;
+    float m = m_ * m_ * m_;
+    float s = s_ * s_ * s_;
+    vec3 linear_srgb = vec3(
+         4.0767416621 * l - 3.3077115913 * m + 0.2309699292 * s,
+        -1.2684380046 * l + 2.6097574011 * m - 0.3413193965 * s,
+        -0.0041960863 * l - 0.7034186147 * m + 1.7076147010 * s
+    );
+    return vec4(
+        clamp(f(linear_srgb.r), 0.0, 1.0),
+        clamp(f(linear_srgb.g), 0.0, 1.0),
+        clamp(f(linear_srgb.b), 0.0, 1.0),
+        oklab.a
+    );
 }
 
-vec3 CrtsMask(vec2 pos, float dark) {
-  #ifdef CRTS_MASK_GRILLE
-  vec3 m = vec3(dark, dark, dark);
-  float x = fract(pos.x * (1.0 / 3.0));
-  if (x < (1.0 / 3.0)) m.r = 1.0;
-  else if (x < (2.0 / 3.0)) m.g = 1.0;
-  else m.b = 1.0;
-  return m;
-  #endif
+//-----------------------------------------------------------------------------
+// Effect Generation Functions
+//-----------------------------------------------------------------------------
 
-  #ifdef CRTS_MASK_GRILLE_LITE
-  vec3 m = vec3(1.0, 1.0, 1.0);
-  float x = fract(pos.x * (1.0 / 3.0));
-  if (x < (1.0 / 3.0)) m.r = dark;
-  else if (x < (2.0 / 3.0)) m.g = dark;
-  else m.b = dark;
-  return m;
-  #endif
+// Bloom sample points
+const vec3[24] samples = {
+    vec3(0.1693761725038636, 0.9855514761735895, 1),
+    vec3(-1.333070830962943, 0.4721463328627773, 0.7071067811865475),
+    vec3(-0.8464394909806497, -1.51113870578065, 0.5773502691896258),
+    vec3(1.554155680728463, -1.2588090085709776, 0.5),
+    vec3(1.681364377589461, 1.4741145918052656, 0.4472135954999579),
+    vec3(-1.2795157692199817, 2.088741103228784, 0.4082482904638631),
+    vec3(-2.4575847530631187, -0.9799373355024756, 0.3779644730092272),
+    vec3(0.5874641440200847, -2.7667464429345077, 0.35355339059327373),
+    vec3(2.997715703369726, 0.11704939884745152, 0.3333333333333333),
+    vec3(0.41360842451688395, 3.1351121305574803, 0.31622776601683794),
+    vec3(-3.167149933769243, 0.9844599011770256, 0.30151134457776363),
+    vec3(-1.5736713846521535, -3.0860263079123245, 0.2886751345948129),
+    vec3(2.888202648340422, -2.1583061557896213, 0.2773500981126146),
+    vec3(2.7150778983300325, 2.5745586041105715, 0.2672612419124244),
+    vec3(-2.1504069972377464, 3.2211410627650165, 0.2581988897471611),
+    vec3(-3.6548858794907493, -1.6253643308191343, 0.25),
+    vec3(1.0130775986052671, -3.9967078676335834, 0.24253562503633297),
+    vec3(4.229723673607257, 0.33081361055181563, 0.23570226039551587),
+    vec3(0.40107790291173834, 4.340407413572593, 0.22941573387056174),
+    vec3(-4.319124570236028, 1.159811599693438, 0.22360679774997896),
+    vec3(-1.9209044802827355, -4.160543952132907, 0.2182178902359924),
+    vec3(3.8639122286635708, -2.6589814382925123, 0.21320071635561041),
+    vec3(3.3486228404946234, 3.4331800232609, 0.20851441405707477),
+    vec3(-2.8769733643574344, 3.9652268864187157, 0.20412414523193154)
+};
 
-  #ifdef CRTS_MASK_NONE
-  return vec3(1.0, 1.0, 1.0);
-  #endif
-
-  #ifdef CRTS_MASK_SHADOW
-  pos.x += pos.y * 3.0;
-  vec3 m = vec3(dark, dark, dark);
-  float x = fract(pos.x * (1.0 / 6.0));
-  if (x < (1.0 / 3.0)) m.r = 1.0;
-  else if (x < (2.0 / 3.0)) m.g = 1.0;
-  else m.b = 1.0;
-  return m;
-  #endif
+// Chromatic aberration time variation
+float offsetFunction(float iTime) {
+    float amount = 1.0;
+    const float periods[4] = {6.0, 16.0, 19.0, 27.0};
+    for (int i = 0; i < 4; i++) {
+        amount *= 1.0 + 0.5 * sin(iTime*periods[i]);
+    }
+    return amount * periods[3];
 }
 
-vec3 CrtsFilter(
-  vec2 ipos,
-  vec2 inputSizeDivOutputSize,
-  vec2 halfInputSize,
-  vec2 rcpInputSize,
-  vec2 rcpOutputSize,
-  vec2 twoDivOutputSize,
-  float inputHeight,
-  vec2 warp,
-  float thin,
-  float blur,
-  float mask,
-  vec2 tone
-) {
-  // Optional apply warp
-  vec2 pos;
-  #ifdef CRTS_WARP
-  // Convert to {-1 to 1} range
-  pos = ipos * twoDivOutputSize - vec2(1.0, 1.0);
-
-  // Distort pushes image outside {-1 to 1} range
-  pos *= vec2(
-      1.0 + (pos.y * pos.y) * warp.x,
-      1.0 + (pos.x * pos.x) * warp.y);
-
-  // TODO: Vignette needs optimization
-  float vin = 1.0 - (
-      (1.0 - CrtsSatF1(pos.x * pos.x)) * (1.0 - CrtsSatF1(pos.y * pos.y)));
-  vin = CrtsSatF1((-vin) * inputHeight + inputHeight);
-
-  // Leave in {0 to inputSize}
-  pos = pos * halfInputSize + halfInputSize;
-  #else
-  pos = ipos * inputSizeDivOutputSize;
-  #endif
-
-  // Snap to center of first scanline
-  float y0 = floor(pos.y - 0.5) + 0.5;
-  // Snap to center of one of four pixels
-  float x0 = floor(pos.x - 1.5) + 0.5;
-
-  // Inital UV position
-  vec2 p = vec2(x0 * rcpInputSize.x, y0 * rcpInputSize.y);
-  // Fetch 4 nearest texels from 2 nearest scanlines
-  vec3 colA0 = CrtsFetch(p);
-  p.x += rcpInputSize.x;
-  vec3 colA1 = CrtsFetch(p);
-  p.x += rcpInputSize.x;
-  vec3 colA2 = CrtsFetch(p);
-  p.x += rcpInputSize.x;
-  vec3 colA3 = CrtsFetch(p);
-  p.y += rcpInputSize.y;
-  vec3 colB3 = CrtsFetch(p);
-  p.x -= rcpInputSize.x;
-  vec3 colB2 = CrtsFetch(p);
-  p.x -= rcpInputSize.x;
-  vec3 colB1 = CrtsFetch(p);
-  p.x -= rcpInputSize.x;
-  vec3 colB0 = CrtsFetch(p);
-
-  // Vertical filter
-  // Scanline intensity is using sine wave
-  // Easy filter window and integral used later in exposure
-  float off = pos.y - y0;
-  float pi2 = 6.28318530717958;
-  float hlf = 0.5;
-  float scanA = cos(min(0.5, off * thin) * pi2) * hlf + hlf;
-  float scanB = cos(min(0.5, (-off) * thin + thin) * pi2) * hlf + hlf;
-
-  // Horizontal kernel is simple gaussian filter
-  float off0 = pos.x - x0;
-  float off1 = off0 - 1.0;
-  float off2 = off0 - 2.0;
-  float off3 = off0 - 3.0;
-  float pix0 = exp2(blur * off0 * off0);
-  float pix1 = exp2(blur * off1 * off1);
-  float pix2 = exp2(blur * off2 * off2);
-  float pix3 = exp2(blur * off3 * off3);
-  float pixT = CrtsRcpF1(pix0 + pix1 + pix2 + pix3);
-
-  #ifdef CRTS_WARP
-  // Get rid of wrong pixels on edge
-  pixT *= max(MIN_VIN, vin);
-  #endif
-
-  scanA *= pixT;
-  scanB *= pixT;
-
-  // Apply horizontal and vertical filters
-  vec3 color =
-    (colA0 * pix0 + colA1 * pix1 + colA2 * pix2 + colA3 * pix3) * scanA +
-      (colB0 * pix0 + colB1 * pix1 + colB2 * pix2 + colB3 * pix3) * scanB;
-
-  // Apply phosphor mask
-  color *= CrtsMask(ipos, mask);
-
-  // Tonal control, start by protecting from /0
-  float peak = max(1.0 / (256.0 * 65536.0),
-      CrtsMax3F1(color.r, color.g, color.b));
-  // Compute the ratios of {R,G,B}
-  vec3 ratio = color * CrtsRcpF1(peak);
-  // Apply tonal curve to peak value
-  peak = peak * CrtsRcpF1(peak * tone.x + tone.y);
-  // Reconstruct color
-  return ratio * peak;
-}
-
-float ToSrgb1(float c) {
-  return (c < 0.0031308 ? c * 12.92 : 1.055 * pow(c, 0.41666) - 0.055);
-}
-vec3 ToSrgb(vec3 c) {
-  return vec3(
-    ToSrgb1(c.r), ToSrgb1(c.g), ToSrgb1(c.b));
-}
+//-----------------------------------------------------------------------------
+// Main Shader
+//-----------------------------------------------------------------------------
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-  float aspect = iResolution.x / iResolution.y;
-  fragColor.rgb = CrtsFilter(
-      fragCoord.xy,
-      vec2(1.0),
-      iResolution.xy * SCALE * 0.5,
-      1.0 / (iResolution.xy * SCALE),
-      1.0 / iResolution.xy,
-      2.0 / iResolution.xy,
-      iResolution.y,
-      vec2(1.0 / (50.0 * aspect), 1.0 / 50.0),
-      INPUT_THIN,
-      INPUT_BLUR,
-      INPUT_MASK,
-      CrtsTone(INPUT_THIN, INPUT_MASK)
-    );
+    vec2 uv = fragCoord.xy / iResolution.xy;
 
-  // Linear to SRGB for output.
-  fragColor.rgb = ToSrgb(fragColor.rgb);
+    // Apply Chromatic Aberration with alpha
+    float amount = offsetFunction(iTime);
+    vec4 colR = texture(iChannel0, vec2(uv.x-ABBERATION_FACTOR*amount / iResolution.x, uv.y));
+    vec4 colG = texture(iChannel0, uv);
+    vec4 colB = texture(iChannel0, vec2(uv.x+ABBERATION_FACTOR*amount / iResolution.x, uv.y));
+
+    // Combine colors while preserving alpha
+    vec4 col = vec4(colR.r, colG.g, colB.b, colG.a);
+
+    // Process Colors and Apply Glow
+    vec4 splittedColor = col;
+    vec4 source = toOklab(splittedColor);
+    vec4 dest = source;
+
+    if (source.x > DIM_CUTOFF) {
+        dest.x *= BRIGHT_BOOST;
+    } else {
+        vec2 step = vec2(1.414) / iResolution.xy;
+        vec3 glow = vec3(0.0);
+        for (int i = 0; i < 24; i++) {
+            vec3 s = samples[i];
+            float weight = s.z;
+            vec4 c = toOklab(texture(iChannel0, uv + s.xy * step));
+            if (c.x > DIM_CUTOFF) {
+                glow.yz += c.yz * weight * COLOR_GLOW;
+                if (c.x <= BRIGHT_CUTOFF) {
+                    glow.x += c.x * weight * DIM_GLOW;
+                } else {
+                    glow.x += c.x * weight * BRIGHT_GLOW;
+                }
+            }
+        }
+        dest.xyz += glow.xyz;
+    }
+
+    vec4 processedColor = toRgb(dest);
+
+    // Apply Scanlines
+    float scanline = abs(sin(fragCoord.y) * SCANLINE_DENSITY * SCANLINE_INTENSITY);
+
+    // Apply Dot Matrix
+    vec2 mask_pos = fragCoord.xy * MASK_SIZE;
+    float mask = 1.0 - (MASK_INTENSITY * (
+        0.5 + 0.5 * sin(mask_pos.x * 3.14159) *
+        sin(mask_pos.y * 3.14159)
+    ));
+
+    // Combine Effects while preserving alpha
+    vec3 final_color = processedColor.rgb * mask;
+    vec3 with_scanline = mix(final_color, vec3(0.0), scanline);
+
+    // Apply global opacity while preserving the original alpha relationship
+    float final_alpha = processedColor.a * GLOBAL_OPACITY;
+
+    fragColor = vec4(with_scanline, final_alpha);
 }
+
